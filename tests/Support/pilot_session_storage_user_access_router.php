f370f305c1db580c616f8320ef03d2843f8b256c
antropophag <antropophag@gmail.com>
2026-09-03 22:30:06 +0300

    bind complete UserAccess dependency config red

--- tests/Support/pilot_session_storage_user_access_router.php
+++ tests/Support/pilot_session_storage_user_access_router.php
@@ -7,6 +7,7 @@ putenv('FMONITOR_SHLZ_CSS_PATH=' . $shlzCss);
 putenv('FMONITOR_PILOT_CSS_PATH=' . $root . '/app/PilotHttp/pilot.css');
 putenv('FMONITOR_ARTIFACT_STORAGE_ROOT=' . (string) getenv('FMONITOR_SESSION_STATE_ROOT') . '/artifacts');
 putenv('FMONITOR_NOW=2026-09-03T12:00:00+03:00');
+putenv('FMONITOR_LEGACY_TABLE_PREFIX=');
 spl_autoload_register(static function (string $class) use ($root): void {
     foreach (['FMonitor2\\' => $root . '/app/', 'FMonitor\\IdentityAccess\\' => $root . '/app/IdentityAccess/'] as $prefix => $base) {
         if (!str_starts_with($class, $prefix)) continue;
