a083637a3cc67cd5b60f8b10a9a489c48cc540d5
antropophag <antropophag@gmail.com>
2026-09-04 03:13:32 +0300

    feat: add original upload MariaDB worker

---
+++ app/AssignmentOrderOriginal/AssignmentOrderOriginalVerificationWorkerBootstrap.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types=1);
+namespace FMonitor2\AssignmentOrderOriginal;
+require_once __DIR__.'/ProductionAssignmentOrderOriginalFactory.php';
---
+++ app/AssignmentOrderOriginal/ProductionAssignmentOrderOriginalFactory.php
@@ -0,0 +1,4 @@
+<?php
+declare(strict_types=1);
+namespace FMonitor2\AssignmentOrderOriginal;
+require_once dirname(__DIR__).'/InstallationProcess/MariaDbAssignmentOrderOriginalProductionRuntime.php';
