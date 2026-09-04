32dd3151941f1198e0fdd6ce5ee8ee9e1b851abd
Timofey Grishin <antropophag@MacBook-Air-Timofey.local>
2026-09-05 01:15:55 +0300

    feat: add original upload database setup

---
+++ app/AssignmentOrderOriginal/AssignmentOrderOriginalSchemaMigration.php
@@ -0,0 +1,3 @@
+<?php declare(strict_types=1);
+require_once dirname(__DIR__).'/InstallationProcess/AssignmentOrderOriginalDefinitionSchemaMigration.php';
+require_once dirname(__DIR__).'/InstallationProcess/AssignmentOrderOriginalSchemaMigration.php';
