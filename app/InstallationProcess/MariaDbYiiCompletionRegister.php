f4972f97ef251046490293f1e397bf08a4576297
Timofey Grishin <antropophag@MacBook-Air-Timofey.local>
2026-09-25 02:44:16 +0300

    chore: establish completion register owner seam

---
+++ app/InstallationProcess/MariaDbYiiCompletionRegister.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace FMonitor2\InstallationProcess;
+
+final class MariaDbYiiCompletionRegister
+{
+    public function read(array $filters): array
+    {
+        throw new \LogicException('INTENDED_RED: issue #268 register is not implemented.');
+    }
+}
